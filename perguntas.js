2593fe77318fb77e5e1faafe4cea145cb027254a
Nicolyfelix123 <00001083138790sp@al.educacao.sp.gov.br>
2024-11-05 09:14:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Brasilia'
+)
+
+criaCartao(
+    'História',
+    'Em qual país começou a Revolução Industrial?',
+    'Inglaterra'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o acontecimento que desencadeou a ww1?',
+    'O assassinato do Arquiduque Francisco Ferdinando'
+)
+
+criaCartao(
+    'Física',
+    'O que estuda a mecãnica quantica?',
+    'Ramo da física que estuda o comportamento das partículas a nível subatomico'
+)
